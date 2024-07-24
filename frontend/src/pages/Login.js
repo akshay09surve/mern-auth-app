5dc739453cfa37d17254cef76bbb3e3671b43289
kandarp-petnotify <petnotify@innopadsolutions.com>
2024-07-24 18:25:14 +0530

    frontend api integration

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -30,7 +30,7 @@ function Login() {
         }
         try
         {
-            const url = 'http://localhost:8080/auth/login'
+            const url = 'https://mern-auth-app-api.vercel.app/auth/login'
             const response = await fetch(url, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
